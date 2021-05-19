d1e438023bdb4d0d3772ec93e8a79c8a6c49a39e
Welblade <welblade@gmail.com>
2021-05-18 21:26:55 -0300

    First commit.

---
+++ src/main/java/com/github/welblade/diocitiesapi/DioCitiesApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.welblade.diocitiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioCitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioCitiesApiApplication.class, args);
+	}
+
+}
